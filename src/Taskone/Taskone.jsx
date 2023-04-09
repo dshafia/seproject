e223b5fab200980249f93c556012b2ced4f399e4
SHAFIA DUDEKULA <shafiadudekula@shafias-air.notrust.clients.wireless.pfw.edu>
2023-04-09 15:35:41 -0400

    Task one and Game one

---
+++ src/Taskone/Taskone.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Template from '../Template/Template'
+import './Taskone.css'
+import { Link } from "react-router-dom";
+import { FiChevronsRight } from 'react-icons/fi';
+
+const QuestionAnswer = () => {
+    return (
+        <div className="ques-container">
+            <p className="ques-text">Question text goes here.</p>
+            <textarea className="ans-text" placeholder="eg. text"></textarea>
+        </div>
+    )
+}
+
+const Taskone = () => {
+    return (
+        <>
+            <Template title="TASK ONE TITLE" />
+            <div className="taskone-content">
+                <div className="taskone-container">
+                    <QuestionAnswer />
+                    <QuestionAnswer />
+                </div>
+                <div className="btn-container">
+                    <Link to="/game" className="taskone-btn"><FiChevronsRight /></Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Taskone
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Gameone from './Gameone/Gameone';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/game",
+    element: <Gameone />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
